a7aeaefe46e92c6386fbdb73fbf6a1efb18ef1f0
Jennifer <je8720su-s@student.lu.se>
2023-04-19 11:01:02 +0200

    server with express

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const wiki = require("./wiki.js");
+const express = require("express");
+const app = express();
+const port = 3003;
+
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+app.use("/wiki", wiki);
