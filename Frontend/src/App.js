8ed2489beda36d251bc070eb7cd9345526d84ad2
Hiba Qutbuddin Habib <hibqut1@gmail.com>
2023-04-19 11:44:57 +0200

    Squashed commit of the following:
    commit ba38d1b2b608962808dc22aef3f0d3e52a1add5c
    Author: Hiba Qutbuddin Habib <hibqut1@gmail.com>
    Date:   Wed Apr 19 11:38:29 2023 +0200
    
        init

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Logoutbutton from './Logoutbutton';
 import Sidebar from './Sidebar';
 import Scorebar from './Scorebar';
-import Lab1 from './Lab1';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
 
-/*const App = () => {
-  const scores = 70;
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <Scorebar scores={scores} />
-      <Logoutbutton />
-      <Sidebar />
-      <Lab1 />
-    </div>
-  );
-};
-
-export default App;*/
 
 function App() {
   const scores = 70;
