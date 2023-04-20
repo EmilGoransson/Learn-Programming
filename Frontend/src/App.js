6a512c29360b9041af83bf9ec913ef4355be3874
Emil Göransson <98598009+EmilGoransson@users.noreply.github.com>
2023-04-20 13:55:54 +0200

    cleaned up code

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import CodeRunner from "./CodeRunner/Presenter/codeRunnerPresenter";
-import CodeRunnerView from "./CodeRunner/View/codeRunnerView";
-import Quiz from "react-quiz-component";
-import QuizComp from "./quiz";
+import CodeRunner from "./Components/CodeRunner/Presenter/codeRunnerPresenter";
 
 function App() {
-  return (
-    <CodeRunner
-      testCases={6}
-      input={3}
-      preText={`
-class Progman
-{  
-
-    public static void main(String[] args) {
-        System.out.println("Does it work?");
-    }
-    
-}`}
-    />
-  );
+  return <div className="App"> test </div>;
 }
 
 export default App;
