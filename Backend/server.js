c8b10fccc2abef705f02f4fab46e21cde198ecb7
Jennifer <je8720su-s@student.lu.se>
2023-04-20 10:45:13 +0200

    test push

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,10 @@ app.get("/", function (req, res) {
     res.send("Hello World!");
 });
 
+app.get("/api", function (req, res) {
+  res.send("Hello World!");
+});
+
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
