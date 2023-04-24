8168178f4a1f182c08f7fc8fee61fb04910b61e4
Emil Göransson <98598009+EmilGoransson@users.noreply.github.com>
2023-04-24 15:49:02 +0200

    errorr fix

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,3 @@
-
-
-
 import React from "react";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Progress from "./Components/Sidebar/Progress";
@@ -11,15 +8,14 @@ import "./Components/LevelRendering/ButtonContainer.css";
 import CodeRunner from "./Components/CodeRunner/Presenter/codeRunnerPresenter";
 import "bootstrap/dist/css/bootstrap.min.css";
 import QuizTemp from "./Components/Quizes/quiz";
-import {lab1Levels} from "./Components/LevelRendering/Levels/lab1";
-import {lab2Levels} from "./Components/LevelRendering/Levels/lab2";
-import {lab3Levels} from "./Components/LevelRendering/Levels/lab3";
-import {mainString} from "./Components/CodeRunner/mainClassString";
-
-
+import { lab1Levels } from "./Components/LevelRendering/Levels/lab1";
+import { lab2Levels } from "./Components/LevelRendering/Levels/lab2";
+import { lab3Levels } from "./Components/LevelRendering/Levels/lab3";
+import { mainString } from "./Components/CodeRunner/mainClassString";
+import { BrowserRouter } from "react-router-dom";
+import MainContent from "./pages/MainContent";
 
 function App() {
-   
   const [message, setMessage] = React.useState(null);
   React.useEffect(() => {
     fetch("http://localhost:3003/api")
@@ -29,21 +25,16 @@ function App() {
 
   return (
     <BrowserRouter>
-    <div className="App" id="outer-container">
-      
-      <Sidebar/>
-      <main id="page-wrap">
-      <MainContent/>
-      </main>
+      <div className="App" id="outer-container">
+        <Sidebar />
+        <main id="page-wrap">
+          <MainContent />
+        </main>
       </div>
-     
-      
-      {message}
-      
 
+      {message}
     </BrowserRouter>
   );
 }
 
 export default App;
-
