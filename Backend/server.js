e5a5164dbb3ce5fb3aae37890452b84ec5b525fa
Jonas Dåderman <Jonasdad@kth.se>
2023-04-24 16:06:32 +0200

    Server DB communication complete

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+const { pool } = require("./dbConfig")
+const dotenv = require("dotenv")
+dotenv.config();
+
 const port = 3003;
 
 app.use(cors());
@@ -13,12 +20,34 @@ app.get("/api", function (req, res) {
   res.json({message: "Hello from server!"});
 });
 
+app.get("/dummy", function(req, res) {
+  res.sendFile(__dirname + "/dummy.html")
+})
+
+app.post("/dummy", async (req, res) => {
+  let {email, password, firstname, lastname} = req.body;
+  pool.query(
+    `INSERT INTO student(email, password, first_name, last_name) 
+     values($1, $2, $3, $4)`, [email, password, firstname, lastname], 
+    (err, results) =>{
+      console.log(email, password,firstname,lastname)
+      if(err){
+       console.log()
+      }
+      else{
+        console.log(results.rows)
+      }
+    } 
+    
+    )
+})
+
 app.get("/test", (req, res) => {
   res.json({ message: "Button pushed!" });
 });
 
 app.listen(port, function () {
-  console.log("App listening on port ${port}!");
+  console.log(`App listening on port ${port}!`);
 });
 
 app.use(express.json());
