7285bbfc26b0c9c18036ea2d00fe8ef347a561e7
Jennifer <je8720su-s@student.lu.se>
2023-04-25 11:13:35 +0200

    get data from json in server, send to app.js, send to a page

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,25 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const port = 3003;
+const fs = require('fs');
 
 app.use(cors());
 
 app.get("/", function (req, res) {
     res.send("Hello World!");
+});
+
+app.get('/mypage', (req, res) => {
+  const jsonData = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
+  res.send(jsonData);
+});
+
+app.get('/users/:userId', (req, res) => {
+  const userId = 1;/*req.params.userId;*/                               // get the userId parameter from the URL or session...
+  const userData = JSON.parse(fs.readFileSync('./data.json', 'utf-8')); // read the JSON data from the file
+  const user = userData.users.find((u) => u.userId === userId);         // find the user object with the specified ID
+  const firstName = user.firstName;                                     // access the firstName property of the user object
+  res.send({ firstName }); 
 });
 
 app.get("/api", function (req, res) {
@@ -18,7 +32,7 @@ app.get("/test", (req, res) => {
 });
 
 app.listen(port, function () {
-  console.log("App listening on port ${port}!");
+  console.log(`App listening on port ${port}!`);
 });
 
 app.use(express.json());
