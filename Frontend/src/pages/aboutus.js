58a5e2ad5f1fa41fdee3cb36082ea60a2f88f72e
Emil Göransson <98598009+EmilGoransson@users.noreply.github.com>
2023-04-25 11:07:55 +0200

    introduced global variable, added store, created lab-1.1

--- Frontend/src/pages/aboutus.js
+++ Frontend/src/pages/aboutus.js
@@ -1,7 +1,10 @@
-const Aboutus = (props) =>{
-    const {title, hej} = props;
-    return<h1> this is we {title} {hej}</h1>;
-
-
+const Aboutus = (props) => {
+  const { title, hej } = props;
+  return (
+    <h1>
+      {" "}
+      this is we {title} {hej}
+    </h1>
+  );
 };
 export default Aboutus;
