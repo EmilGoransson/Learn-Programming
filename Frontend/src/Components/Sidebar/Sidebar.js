7a6e5f184350420283bea997f6537304c13d08ef
RasmussBrodin <rasmuss.brodin@gmail.com>
2023-04-26 11:55:21 +0200

    Squashed commit of the following:
    commit 3a4e4488bb5c6248320941fa9a85707a08d4b6d7
    Author: RasmussBrodin <rasmuss.brodin@gmail.com>
    Date:   Wed Apr 26 11:55:08 2023 +0200
    
        sidebar logo
    
    commit 19337e2b3360ba2f2349bf6f837912b4179d8dde
    Author: RasmussBrodin <rasmuss.brodin@gmail.com>
    Date:   Wed Apr 26 08:54:48 2023 +0200
    
        routing in assigment1

--- Frontend/src/Components/Sidebar/Sidebar.js
+++ Frontend/src/Components/Sidebar/Sidebar.js
@@ -3,31 +3,37 @@ import "./Sidebar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUser,
-  faFlask,
+  faLaptop,
   faBook,
-  faPencilAlt,
+  faPencil,
   faUsers,
 } from "@fortawesome/free-solid-svg-icons";
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {NavLink } from "react-router-dom";
+import { ReactComponent as LogoUser } from "./user.svg"
+import { ReactComponent as LogoLab } from "./terminal.svg"
+import { ReactComponent as LogoExam } from "./edit-2.svg"
+import { ReactComponent as LogoTheory } from "./book.svg"
+import { ReactComponent as LogoUs } from "./users.svg"
 
 const Sidebar = () => {
   return (
-    <div className="sidenav shadow-xl">
+    <div className="sidenav">
       <img src="https://i.imgur.com/5sZenqF.png" alt="My Image" />
-      <NavLink to="/profile" activeClassName="active">
-        <FontAwesomeIcon icon={faUser} /> Profile
-      </NavLink>
       <NavLink to="/labs" activeClassName="active">
-        <FontAwesomeIcon icon={faFlask} /> Labs
+        <LogoLab/> Labs
       </NavLink>
       <NavLink to="/exam" activeClassName="active">
-        <FontAwesomeIcon icon={faBook} /> Previous Exams
+        <LogoExam/> Exams
       </NavLink>
       <NavLink to="/theory" activeClassName="active">
-        <FontAwesomeIcon icon={faPencilAlt} /> Theory
+        <LogoTheory/> Theory
+      </NavLink>
+      <NavLink to="/profile" activeClassName="active">
+        <LogoUser/> 
+        Profile
       </NavLink>
       <NavLink to="/aboutus" activeClassName="active">
-        <FontAwesomeIcon icon={faUsers} /> About us
+        <LogoUs/> About us
       </NavLink>
     </div>
   );
