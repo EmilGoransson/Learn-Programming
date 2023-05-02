fcff56cc58742985023d6d9e96c5389dfb4608a7
ayahbaya <ayahb@kth.se>
2023-05-02 14:27:54 +0200

    Fixed Theory and scrolling bar

--- Frontend/src/Components/Scrollingbar/scrollingbar.js
+++ Frontend/src/Components/Scrollingbar/scrollingbar.js
@@ -6,110 +6,105 @@ const Scrollingbar = () => {
   return (
     <div className="sidebar no-scroll-bar">
       <ul>
-        <div className="bg-lightBlueGray  text-textGray">
+        <div className="bg-custom-color  text-textGray" >
+        <li className="Title"> Theory </li>
           <li>
-            <li>Theory</li>
-            <NavLink to="/Arrays" activeClassName="active">
+          <NavLink to="/Variabels" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
-
+        
           <li>
-          <NavLink to="/Varibels" activeClassName="active">
-              Arrays
-            </NavLink>
-          </li>
-          <li>
-          <NavLink to="/Item3" activeClassName="active">
+          <NavLink to="/Item3" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/Item4" activeClassName="active">
+          <NavLink to="/Item4" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/Item 5" activeClassName="active">
+          <NavLink to="/Item 5" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/Item6" activeClassName="active">
+          <NavLink to="/Item6" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/Item7" activeClassName="active">
+          <NavLink to="/Item7" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/Item8" activeClassName="active">
+          <NavLink to="/Item8" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
-          <li><NavLink to="/Item9" activeClassName="active">
+          <li><NavLink to="/Item9" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           
           <li>
-          <NavLink to="/item10" activeClassName="active">
+          <NavLink to="/item10" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/item11" activeClassName="active">
+          <NavLink to="/item11" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/item12" activeClassName="active">
+          <NavLink to="/item12" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
 
           <li>
-            <NavLink to="/item13" activeClassName="active">
+            <NavLink to="/item13" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
 
           </li>
           <li>
-          <NavLink to="/item14" activeClassName="active">
+          <NavLink to="/item14" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/item15" activeClassName="active">
+          <NavLink to="/item15" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/item16" activeClassName="active">
+          <NavLink to="/item16" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/item17" activeClassName="active">
+          <NavLink to="/item17" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/item18" activeClassName="active">
+          <NavLink to="/item18" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
           <li>
-          <NavLink to="/item19" activeClassName="active">
+          <NavLink to="/item19" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
 
           </li>
 
           <li>
-          <NavLink to="/item20" activeClassName="active">
+          <NavLink to="/item20" activeClassName="active"className="my-Link">
               Arrays
             </NavLink>
           </li>
