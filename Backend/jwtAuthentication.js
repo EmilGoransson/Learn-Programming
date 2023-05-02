156140dc523440e38df02b58c2eda890465b027d
Jonas Dåderman <Jonasdad@kth.se>
2023-05-02 09:05:07 +0200

    Merge backend into main

---
+++ Backend/jwtAuthentication.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const router = express.Router();
+const pool = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const jwtGenerator = require("./jwtGenerator");
+const authorization = require("./middleware/authorization")
+
+//register route
+router.post("/Signup", async (req,res) => {
+    try{
+        //1. destructure req.body (first_name, last_name, email, password)
+            const {email, firstName, lastName, password} = req.body;
+            //2. check if user exists
+            //this is a temporary psql query that will be found in queries.js later on
+            const user =  await pool.query("SELECT * FROM student WHERE email = $1", [email]);
+            if(user.rows.length > 0){
+                return res.status(409).json({error: "User already exists"});
+            }
+            
+        //3. if user not exists -> encrypt password
+            const salt = await bcrypt.genSalt(10);
+            const encryptedPassword = await bcrypt.hash(password, salt);
+        //4. insert user into DB
+            //This is a temporary psql query that will be in queries.js later on
+            let new_user = await pool.query("INSERT INTO student(first_name, last_name, email, password) VALUES($1,$2,$3,$4)", 
+            [firstName, lastName, email, encryptedPassword]);
+
+        //5. generate JWT token
+        const token = jwtGenerator(user.rows[2]);
+        res.json({token})
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
+
+
+//login route
+router.post("/login", async (req,res) =>{
+    try{
+        let {email, password} = req.body;
+        const user = await pool.query("SELECT * FROM student WHERE email = $1", [email]);
+        if(user.rows.length === 0){
+            return res.status(401).send("Incorrect Email or Password");
+        }
+        const validPassword = await bcrypt.compare(password, user.rows[0].password);
+        if(!validPassword){
+            return res.status(401).send("Incorrect Email or Password");
+        }
+
+        const cLevel = await pool.query("SELECT s_id, current_level, first_name from student where email = $1", [email])
+        currentLevel = cLevel.rows[0].current_level;
+        userName = cLevel.rows[0].first_name;
+        id = cLevel.rows[0].s_id;
+
+        const token = jwtGenerator(user.rows[2]);
+        res.json({ token, currentLevel, userName, id});
+        console.log("User " + id + " logged in successfully");
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
+
+
+router.post("/profile", authorization, async(req,res) =>{
+
+    const remove = pool.query("DELETE FROM student WHERE s_id = $1", [s_id])
+})
+
+router.post("/profile", authorization, async(req,res) =>{
+
+    let {email, password, firstName, lastName} = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const encryptedPassword = await bcrypt.hash(password, salt);
+
+
+
+    const changes = pool.query(
+        "UPDATE student SET email = $1, SET password = $2, SET first_name = $3, SET last_name = $4;",
+        [email, encryptedPassword, firstName, lastName]
+    )
+})
+//verify token
+router.get("/verify", authorization, async (req,res) =>{
+    try{
+        res.json(true)
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
+
+
+module.exports = router;
---
+++ Frontend/src/Components/LoginPage/Login.js
@@ -0,0 +1,98 @@
+import React, { Fragment, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import "./LoginPage.css";
+import { toast } from "react-toastify";
+import useLevelStore from "../../Model/frontEndStore";
+
+const Login = ({ setAuth }) => {
+  const getLevel = useLevelStore(state => state.getLevel);
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = inputs;
+
+  const onChange = (e) =>
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { email, password };
+      const response = await fetch(
+        "http://localhost:3003/authentication/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(body),
+        }
+      );
+
+      const parseRes = await response.json();
+      if (parseRes.token) {
+        localStorage.setItem("token", parseRes.token);
+        localStorage.setItem("id", parseRes.id)
+        localStorage.setItem("name", parseRes.userName)
+        setAuth(true);
+        toast.success("Logged in Successfully");
+      } else {
+        setAuth(false);
+        toast.error(parseRes);
+      }
+
+      getLevel(Number(parseRes.currentLevel));
+      //getLevel(5);
+} catch (err) {
+      console.error(err.message);
+    }
+  };
+  return (
+    <Fragment>
+      <div class="container">
+        <div class="form-container">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "2rem",
+            }}
+          >
+            {/*<img src={logo} alt="Logo" className="hej" />*/}
+          </div>
+          <h2 style={{ alignSelf: "flex-start" }}>Log in</h2>
+          <br></br>
+          <form onSubmit={onSubmitForm}>
+            <input
+              type="text"
+              name="email"
+              value={email}
+              placeholder="E-mail"
+              onChange={(e) => onChange(e)}
+              className="ruta"
+            />
+            <input
+              type="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => onChange(e)}
+              className="ruta"
+            />
+            <button type="submit" className="loginbutton">
+              Log in
+            </button>
+          </form>
+          <p>
+            Don’t have an account? Click <a href="/createaccount">here</a> to get
+            one.
+          </p>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Login;
