66ff9f8e5b1f21dbc6455d0f6fa751144963bcc3
RasmussBrodin <rasmuss.brodin@gmail.com>
2023-05-03 16:43:56 +0200

    clean app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -17,19 +17,11 @@ import CreateAccount from "./Components/SignupPage/SignupPage";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
 import TopBar from "./Components/topBar/topBarPresenter";
 import RightSideBar from "./Components/rightSideBar/rightSideBarPresenter";
-import CurrentProgressBarPresenter from "./Components/CurrentProgressBar/Presenter/currentProgressBarPresenter";
 import Scrollingbar from "./Components/Scrollingbar/scrollingbar";
 import PinnedList from "./Components/PinnedList/PinnedList";
-import Logoutbutton from "./Logoutbutton";
 import Progress from "./Components/CurrentProgressBar/Presenter/currentProgressBarPresenter";
-import useLevelStore from "./Model/frontEndStore";
 
 function App() {
-  const [message, setMessage] = React.useState(null);
-  const [firstName, setFirstName] = React.useState(null);
-  const [lastName, setLastName] = React.useState(null);
-  const [email, setEmail] = React.useState(null);
-  const [JSONmessage, setJSONmessage] = React.useState(null);
   const checkAuthenticated = async () => {
     try {
       const res = await fetch(
@@ -55,57 +47,13 @@ function App() {
   useEffect(() => {
     checkAuthenticated();
   }, []);
-  useEffect(() => {
-    checkAuthenticated();
-  }, []);
+
   // Default state of authentication is false
   const [isAuthenticated, setIsAuthenticated] = React.useState(false);
 
   const setAuth = (boolean) => {
     setIsAuthenticated(boolean);
   };
-  React.useEffect(() => {
-    Promise.all([
-      fetch(`http://localhost:3003/users/:userId`).then((res) => res.json()), //Change (wildcard) userID something like: `http://localhost:3003/users/${userid}`
-      fetch("http://localhost:3003/mypage").then((res) => res.json()),
-      //fetch("http://localhost:3003/api").then((res) => res.json())
-    ]).then(([data1, data2, data3]) => {
-      setFirstName(data1.firstName);
-      setLastName(data1.lastName);
-      setEmail(data1.email);
-      setJSONmessage(data2.JSONmessage);
-      //setMessage(data3.message)
-    });
-  }, []);
-
-  React.useEffect(() => {
-    fetch("http://localhost:3003/mypage")
-      .then((res) => res.json())
-      .then((data) => setJSONmessage(data.JSONmessage));
-  }, []);
-
-  //const userid = "Lab1Assignments";
-  React.useEffect(() => {
-    fetch(`http://130.229.172.67:3003/users/:userId`) //Change (wildcard) userID something like               : `http://localhost:3003/users/${userid}`
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((data) => setFirstName(data.firstName))
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-      });
-  }, []);
-
-  /*React.useEffect(() => {
-    fetch("http://localhost:3003/mypage")
-      .then((res) => res.json())
-      .then((data) => setJSONmessage(data.JSONmessage));
-      .then((data) => setTesting(data.testing));
-  }, []);*/
-
 
 return (
   <BrowserRouter>
