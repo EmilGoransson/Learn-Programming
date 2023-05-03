090c040a2537582a135ad453a559c3775e78cdbd
Emil Göransson <98598009+EmilGoransson@users.noreply.github.com>
2023-05-03 10:57:13 +0200

    removed duplicate files

--- Frontend/src/Components/rightSideBar/rightSideBarView 2.js
+++
@@ -1,9 +0,0 @@
-function rightSideBarView(props) {
-  return (
-    <div
-      className="fixed h-screen   bg-[#14161B] right-0 "
-      style={{ width: 15 + "%" }}
-    ></div>
-  );
-}
-export default rightSideBarView;
