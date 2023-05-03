46adba3bec910caf9dfc79403fcfaa7a2704a7b3
Emil Göransson <98598009+EmilGoransson@users.noreply.github.com>
2023-05-03 11:57:15 +0200

    fixed pinned & added pinning functionallity

--- Frontend/src/pages/Assignments/Lab1Assignments/Assign1/Assignment1Presenter.js
+++ Frontend/src/pages/Assignments/Lab1Assignments/Assign1/Assignment1Presenter.js
@@ -2,6 +2,8 @@ import React from "react";
 import Lab1a1View from "./Assignment1View";
 import useLevelStore from "../../../../Model/frontEndStore";
 import { useEffect } from "react";
+import { ReactComponent as LogoBookmark } from "../../bookmark.svg";
+import { ReactComponent as LogoBookmarkFilled } from "../../filledBookmark.svg";
 
 function Lab1a1() {
   const currentLevel = useLevelStore((state) => state.currentLevel);
@@ -10,8 +12,23 @@ function Lab1a1() {
   //update this variable if using this as template!!
   const contentName = "Variables";
   const thisLevel = 1;
+  const [isFilled, setIsFilled] = React.useState(
+    <LogoBookmarkFilled className="bookmark-icon" />
+  );
 
   const [status, setStatus] = React.useState([true, "next-button-gray"]);
+
+  function isPinned() {
+    if (getPinned.includes(contentName)) {
+      setIsFilled(
+        <LogoBookmarkFilled className="text-textGray h-6 w-6 fill-current" />
+      );
+    } else {
+      setIsFilled(
+        <LogoBookmark className="text-textGray h-6 w-6 fill-current" />
+      );
+    }
+  }
 
   const preMadeText = `
   import java.util.Scanner;  // Import the Scanner class
@@ -39,7 +56,8 @@ class Progman
   }
   useEffect(() => {
     IsDisabled();
-  }, [currentLevel]);
+    isPinned();
+  }, [currentLevel, getPinned]);
 
   return (
     <Lab1a1View
@@ -47,6 +65,7 @@ class Progman
       preMadeText={preMadeText}
       disabled={status}
       thisLevel={thisLevel}
+      svg={isFilled}
     ></Lab1a1View>
   );
 }
