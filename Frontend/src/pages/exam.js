e6a881540689462489815419cdf17c4758a1800c
Julius Larsson <jular@kth.se>
2023-05-04 08:35:23 +0200

    xD

--- Frontend/src/pages/exam.js
+++
@@ -1,9 +0,0 @@
-const Exam = () => {
-  return (
-    <div className="bg-lightBlueGray w-screen h-screen">
-      {" "}
-      <div>test</div>
-    </div>
-  );
-};
-export default Exam;
--- Frontend/src/pages/labs.js
+++
@@ -1,4 +0,0 @@
-const Labs = () => {
-  return <h1> labs page</h1>;
-};
-export default Labs;
