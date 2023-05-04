d212988f810cf2b22d779ac6738514355f09a704
eveberg <131174122+eveberg@users.noreply.github.com>
2023-05-04 14:05:45 +0200

    LIte småfix på exams

--- Frontend/src/pages/exam.js
+++ Frontend/src/pages/exam.js
@@ -17,18 +17,19 @@ function Exam (){
     </div>
     <div className="exam">
       <main>
-        <h1 className = "examHeader">Tips for the exam </h1>
-        <br></br>
-        <h1>1. Practice on old exams</h1>
+        <h1 className = "examHeader">Tips for the Exam </h1>
+        <p> Here we have gathered some of our best tips on how to ace the exam. Follow these tips and start 
+          preparing in good time before the exam and you'll see that you'll ace the exam! </p>
+        <h1>1. Practice on Old Exams</h1>
         <p> Most of the exams in Programming 1 follow a similar structure. By practicing old exams and understanding them
           you'll have a good chance of doing well on your exam.
         </p>
-          <h1>2. Practice writing code on paper</h1>
+          <h1>2. Practice Writing Code on Paper</h1>
           <p>The exam is pen and paper hence no compilator or IDE will help you autofill your code. 
              So do yourself a favor and practice writing code on a paper. When doing this remember to still 
              indent as you would do on a computer so it makes it easier to read.
           </p>
-          <h1>3. Write comments</h1>
+          <h1>3. Write Comments</h1>
           <p> It can be of great help to comment your code even on the exam. Try to include comments 
             explaining what you are thinking and how the code works. This can be of great help if 
             the code that you've written isn't correct hence your comments may help to show that you
@@ -40,17 +41,18 @@ function Exam (){
             code should do and be formatted. So practice on pseudocode so that you have something to fall back on
             in case you need.
           </p>
-          <h1>5. Sorting algoritms</h1>
+          <h1>5. Sorting Algoritms</h1>
           <p> Question 5 on the exam always contains a sorting algoritm and a question about time complexity. Also, in order 
             to pass the exam one needs atleast 3 points on this question. The sorting algoritms are usually either
             selection sort or insertion sort. So look up these algoritms and learn how they work and their time complexity.
             It's good to do this some time before the exam since these can be a little tricky to understand at first.
           </p>
-          <h1>6. Read up</h1>
-          <p> Question 5 on the exam always contains a sorting algoritm and a question about time complexity. Also, in order 
-            to pass the exam one needs atleast 3 points on this question. The sorting algoritms are usually either
-            selection sort or insertion sort. So look up these algoritms and learn how they work and their time complexity.
-            It's good to do this some time before the exam since these can be a little tricky to understand at first.
+          <h1>6. Do this Course</h1>
+          <p> This course is carefully designed to be aligned with the concepts that are included in Programming 1. If you do 
+            this course you should be introduced to all the key concepts. If you have any pinned items at the end of this
+            webcourse make sure to read up on them before the final exam. It's possible to redo old levels and if you only need
+            to fresh up on some theory check out our theory section we're we have gathered all the theory from the different
+            levels!
           </p>
       </main>
     </div>
