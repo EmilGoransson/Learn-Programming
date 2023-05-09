a2a0403239610d8b0815a10c4a86faf6d33ded3d
RasmussBrodin <rasmuss.brodin@gmail.com>
2023-05-09 08:52:05 +0200

    hej

--- Frontend/src/pages/Assignments/Lab2Assignments/Assign15/Assignment15Presenter.js
+++ Frontend/src/pages/Assignments/Lab2Assignments/Assign15/Assignment15Presenter.js
@@ -15,23 +15,28 @@ function Lab2a15() {
   const currentLevel = useLevelStore((state) => state.currentLevel);
   const addPinned = useLevelStore((state) => state.addPinned);
   const getPinned = useLevelStore((state) => state.pinnedTheory);
+  const removePinned = useLevelStore((state) => state.removePinned);
   //update this variable if using this as template!!
   const contentName = "Buffered reader";
   const thisLevel = 15;
   const [isFilled, setIsFilled] = React.useState(
-    <LogoBookmarkFilled className="bookmark-icon" />
+      <LogoBookmarkFilled className="bookmark-icon"/>
   );
 
   const [status, setStatus] = React.useState([true, "next-button-gray"]);
 
+  function scrollToTop() {
+    window.scrollTo(0, 0)
+  };
+
   function isPinned() {
     if (getPinned.includes(contentName)) {
       setIsFilled(
-        <LogoBookmarkFilled className="text-textGray h-6 w-6 fill-current" />
+          <LogoBookmarkFilled className="text-textGray h-6 w-6 fill-current"/>
       );
     } else {
       setIsFilled(
-        <LogoBookmark className="text-textGray h-6 w-6 fill-current" />
+          <LogoBookmark className="text-textGray h-6 w-6 fill-current"/>
       );
     }
   }
