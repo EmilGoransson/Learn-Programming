af12d4ebfa87c0969a4e006a14633b41ab60d918
ayahbaya <ayahb@kth.se>
2023-05-09 15:42:09 +0200

    save level

--- Frontend/src/pages/Assignments/Lab2Assignments/Assign28/Assignment28Presenter.js
+++ Frontend/src/pages/Assignments/Lab2Assignments/Assign28/Assignment28Presenter.js
@@ -17,7 +17,7 @@ function Lab2a28() {
     const getPinned = useLevelStore((state) => state.pinnedTheory);
     //update this variable if using this as template!!
     const contentName = "Boss 1";
-    const thisLevel = 14;
+    const thisLevel = 28;
     const [isFilled, setIsFilled] = React.useState(
         <LogoBookmarkFilled className="bookmark-icon"/>
     );
