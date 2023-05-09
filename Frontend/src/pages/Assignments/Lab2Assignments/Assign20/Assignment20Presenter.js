eb499ed477b9524b97422a4fb848ee742b7af18d
eveberg <131174122+eveberg@users.noreply.github.com>
2023-05-09 14:43:30 +0200

    indentering

--- Frontend/src/pages/Assignments/Lab2Assignments/Assign20/Assignment20Presenter.js
+++ Frontend/src/pages/Assignments/Lab2Assignments/Assign20/Assignment20Presenter.js
@@ -41,20 +41,13 @@ function Lab2a20() {
     }
 
     
-    const preMadeText = `
-    import java.util.Scanner;
-    class Progman
+    const preMadeText = `import java.util.Scanner;
+class Progman
 {  
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+     Scanner scanner = new Scanner(System.in);
     
-
-
-
-
-
     }
-    
 }`;
 
 
