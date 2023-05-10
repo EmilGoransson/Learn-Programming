4c21f48e5138569d400240fe1e3210ec4592ea3a
eveberg <131174122+eveberg@users.noreply.github.com>
2023-05-10 10:38:00 +0200

    lab 37 klar

---
+++ Frontend/src/pages/Assignments/Lab3Assignments/Assign37/Assignment37Presenter.js
@@ -0,0 +1,155 @@
+import React from "react";
+import Lab3a37View from "./Assignment37View";
+import useLevelStore from "../../../../Model/frontEndStore";
+import {useEffect} from "react";
+import {ReactComponent as LogoBookmark} from "../../bookmark.svg";
+import {ReactComponent as LogoBookmarkFilled} from "../../filledBookmark.svg";
+import RightSideBar from "../../../../Components/rightSideBar/rightSideBarPresenter";
+import Progress from "../../../../Components/CurrentProgressBar/Presenter/currentProgressBarPresenter";
+import Sidebar from "../../../../Components/Sidebar/Sidebar";
+import PinnedList from "../../../../Components/PinnedList/PinnedList";
+import TopBar from "../../../../Components/topBar/topBarPresenter";
+
+function Lab3a37() {
+    const currentLevel = useLevelStore((state) => state.currentLevel);
+    const addPinned = useLevelStore((state) => state.addPinned);
+    const removePinned = useLevelStore((state) => state.removePinned);
+    const getPinned = useLevelStore((state) => state.pinnedTheory);
+    //update this variable if using this as template!!
+    const contentName = "From Linkedlist to Array";
+    const thisLevel = 37;
+    const [isFilled, setIsFilled] = React.useState(
+        <LogoBookmarkFilled className="bookmark-icon"/>
+    );
+
+    const [status, setStatus] = React.useState([true, "next-button-gray"]);
+
+
+    function isPinned() {
+        if (getPinned.includes(contentName)) {
+            setIsFilled(
+                <LogoBookmarkFilled className="text-textGray h-6 w-6 fill-current"/>
+            );
+        } else {
+            setIsFilled(
+                <LogoBookmark className="text-textGray h-6 w-6 fill-current"/>
+            );
+        }
+    }
+
+    const preMadeText = `import java.util.Scanner;
+class Progman {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int scanned = scanner.nextInt();
+        
+        //Initializing the list and adding elements to it
+        LinkedList list = new LinkedList();
+        for(int i = 0; i < 5; i++){
+            list.add(5*i*scanned);
+        }
+    
+        //your code here
+        int[] array = new int[....];
+
+        //Printing statement 
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ",");
+        }
+    }
+}
+    
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+    
+class LinkedList {
+    Node head;
+    
+    public LinkedList() {
+        this.head = null;
+    }
+    
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    
+    public int length() {
+        int count = 0;
+        Node current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+    
+    public int get(int index) {
+        if (index < 0 || index >= length()) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+}  
+`;
+
+    function addToPinned() {
+        if (!getPinned.includes(contentName)) {
+            addPinned(contentName);
+        } else {
+            removePinned(contentName);
+        }
+    }
+
+    function IsDisabled() {
+        if (thisLevel >= currentLevel) {
+            setStatus([true, "next-button-gray"]);
+        } else {
+            setStatus([false, "button-lab-cyan "]);
+        }
+    }
+
+    useEffect(() => {
+        IsDisabled();
+        isPinned();
+    }, [currentLevel, getPinned]);
+
+    return (
+        <div>
+            <RightSideBar></RightSideBar>
+            <Sidebar></Sidebar>
+            <Progress/>
+            <PinnedList/>
+            <TopBar></TopBar>
+            <Lab3a37View
+
+                addPinned={addToPinned}
+                preMadeText={preMadeText}
+                disabled={status}
+                thisLevel={thisLevel}
+                svg={isFilled}
+            ></Lab3a37View>
+        </div>
+    );
+}
+
+export default Lab3a37;
