6a46aff112718afb4f026ccb1f63352ffc6f7064
RasmussBrodin <rasmuss.brodin@gmail.com>
2023-05-11 11:05:17 +0200

    hej fix

--- Frontend/src/pages/WelcomePage/WelcomePage.js
+++ Frontend/src/pages/WelcomePage/WelcomePage.js
@@ -1,19 +1,33 @@
 import React from "react";
 import "./WelcomePage.css";
+import { Typewriter } from "react-simple-typewriter";
 import { Link } from "react-router-dom";
 
 function WelcomePage() {
   return (
-    <div className="WelcomePage h-screen">
+    <div className="WelcomePage">
       <img src="https://i.imgur.com/yLqZDJM.png" className="logo" />
+      <img
+          src="https://i.imgur.com/GfPVRJH.png"
+          className="comp"
+          alt="comp"
+        />
       <main>
         <h1>Welcome!</h1>
-        <h2 className="text-[#d63384]">Are you ready to code?</h2>
-        <p>
+        <h2 className="text-[#d63384]">Are you ready to <Typewriter
+            words={[" code?", " learn?", " improve?"]}
+            loop={false}
+            cursor
+            cursorStyle="_"
+            typeSpeed={100}
+            deleteSpeed={50}
+            delaySpeed={1200}
+          /></h2>
+        <p className="text-p">
           {" "}
           This web course is carefully designed to prepare the students for the
           programming assignments in ID1018. The web course includes both theory
-          on different programming subjects and also practice tests where the
+          on different programming subjects and also practice tests &ensp; &ensp;where the
           students get to practice coding.
         </p>
         <div className="buttons">
